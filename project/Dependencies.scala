af79286577299dae2144a4b52e5fbf622c2c180a
slandelle <slandelle@gatling.io>
2025-04-09 21:25:27 +0200

    fix: support non-15 window bits in WebSocket compression extension, close #4619
    Requires the jzlib optional dependency as java.util.zip.Delfater doesn't accept a wbits parameter and uses internally a hardcoded value of 15.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,6 +35,7 @@ object Dependencies {
   private val brotli4cOsXX86                 = brotli4j.withName("native-osx-x86_64")
   private val brotli4cOsXArm                 = brotli4j.withName("native-osx-aarch64")
   private val brotli4jWindows                = brotli4j.withName("native-windows-x86_64")
+  private val jzlib                          = "com.jcraft"                           % "jzlib"                             % "1.1.3"
   private val config                         = "com.typesafe"                         % "config"                            % "1.4.3"
   private val saxon                          = "net.sf.saxon"                         % "Saxon-HE"                          % "12.5"
   private val xmlresolver                    = "org.xmlresolver"                      % "xmlresolver"                       % "6.0.14"
@@ -172,6 +173,7 @@ object Dependencies {
     brotli4cOsXX86,
     brotli4cOsXArm,
     brotli4jWindows,
+    jzlib,
     junit,
     junitEngine,
     junitPlatformLauncher,
